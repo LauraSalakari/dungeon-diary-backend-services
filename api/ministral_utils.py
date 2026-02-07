37ed25c588739d5b0c25d8e29c4de8082051264e
Laura <70233672+LauraSalakari@users.noreply.github.com>
2026-02-07 15:00:35 +0100

    get RAG pipeline working for PHB

--- api/ministral_utils.py
+++ api/ministral_utils.py
@@ -1,2 +1,27 @@
-def get_rag_answer():
-    return "hello"
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+ministral_url = os.getenv("MINISTRAL_URL")
+
+MODEL_ID = "mistralai/Ministral-3-3B-Instruct-2512"
+
+client = OpenAI(
+    base_url=ministral_url,
+    api_key="not-needed"
+)
+
+
+def get_rag_answer(query: str, context):
+    prompt = f"Answer the following question based on the context below:\n\n{context}\n\nQuestion: {query}\nAnswer:"
+
+    resp = client.chat.completions.create(
+        model=MODEL_ID,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant providing answers based on provided context."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return resp.choices[0].message.content
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from api import create_phb_embeddings
+from api import retrieve_answer_phb
 
-create_phb_embeddings()
+# create_phb_embeddings()
+
+a = retrieve_answer_phb("What are the wizard subclasses?")
+print(a)
