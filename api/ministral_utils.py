7210002534aa2871e02b826234d72528409cd3db
Laura <70233672+LauraSalakari@users.noreply.github.com>
2026-02-07 15:58:25 +0100

    start exploring fastAPI

--- api/ministral_utils.py
+++ api/ministral_utils.py
@@ -9,17 +9,24 @@ MODEL_ID = "mistralai/Ministral-3-3B-Instruct-2512"
 
 client = OpenAI(
     base_url=ministral_url,
-    api_key="not-needed"
+    api_key="not-needed"        # currently the Ministral service expects no auth
 )
 
+# Ministral-3 is running locally in a Docker container available at the URL specified in the .env file
 
 def get_rag_answer(query: str, context):
+    """
+    Uses the Ministral end-point to generate an answer based on the provided conxt
+    :param query: User question
+    :param context: Relevant context fetched from vector store
+    :return: Generated answer in string format
+    """
     prompt = f"Answer the following question based on the context below:\n\n{context}\n\nQuestion: {query}\nAnswer:"
 
     resp = client.chat.completions.create(
         model=MODEL_ID,
         messages=[
-            {"role": "system", "content": "You are a helpful assistant providing answers based on provided context."},
+            {"role": "system", "content": "You are a helpful assistant providing answers based only on the provided context."},
             {"role": "user", "content": prompt}
         ]
     )
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from api import create_phb_embeddings
 from api import retrieve_answer_phb
+from fastapi import FastAPI
 
 # create_phb_embeddings()
 
-a = retrieve_answer_phb("What are the wizard subclasses?")
-print(a)
+# a = retrieve_answer_phb("What are the wizard subclasses?")
+# print(a)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
