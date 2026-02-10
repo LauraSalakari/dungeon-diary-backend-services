303b12113b6000002891bca9643e263da90ad5b3
Laura <70233672+LauraSalakari@users.noreply.github.com>
2026-02-10 18:57:49 +0100

    api end point for phb rag works

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+
 from api import create_phb_embeddings
 from api import retrieve_answer_phb
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
 
 # create_phb_embeddings()
 
@@ -9,10 +12,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+class PhbQuery(BaseModel):
+    q: str
+
+@app.post("/phb-rag")
+def query_phb_rag (query: PhbQuery, res: Response):
+    print("Question received:", query)
+    try:
+        res.status_code = status.HTTP_200_OK
+        return retrieve_answer_phb(query.q)
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
